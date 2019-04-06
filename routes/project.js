2ae62beaee923262726f6c7073467d4f7c7b3274
thomasutsey <tomutsey@yahoo.com>
2019-04-06 15:00:51 -0400

    add get by id with project and action
    mvp complete

--- routes/project.js
+++ routes/project.js
@@ -39,4 +39,35 @@ router.get('/', async (req, res)=>{
     res.status(500).json({message, error});
   }
 });
+
+// app.get('/:id', (req, res) => {
+//   Promise.all([
+//     get(`http://localhost:3000/api/user`),
+//     get(`http://localhost:3000/api/books`)
+//   ]).then(([user, {books}]) =>
+//     res.send({
+//       user: user.name,
+//       books
+//     }))
+//     .catch(err => res.send('Ops, something has gone wrong'))
+// })
+
+router.get('/:id', async (req, res)=>{
+  try{
+
+ const result = await db('project').where({ id: req.params.id }).first();
+ const act = await db('action').where({projectID:req.params.id});
+ const info = ({result,act})
+   
+ if(result){
+
+ res.status(200).json(info)
+ }else{
+  res.status(404).json({message: 'Records not found'})
+ }
+  }catch(error){
+    const message = errors[error.errno] || 'Retry different value server error';
+    res.status(500).json({message, error});
+  }
+});
 module.exports = router;
